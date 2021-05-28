395103d1e1b9cce0a43d06da0ac9faf602354db9
Fliedge <filip.aldenhov@gmail.com>
2021-05-28 10:15:41 +0200

    Made spring return App/JSON

--- Backend/PublicAPIBackend/src/main/java/com/example/nlpPublicAPI/controllers/NlpController.java
+++ Backend/PublicAPIBackend/src/main/java/com/example/nlpPublicAPI/controllers/NlpController.java
@@ -2,8 +2,12 @@ package com.example.nlpPublicAPI.controllers;
 
 import com.example.nlpPublicAPI.entities.Keywords;
 import com.example.nlpPublicAPI.services.NlpService;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -20,16 +24,18 @@ public class NlpController {
         return "Hello";
     }
 
-    @GetMapping("/keywords")
+    //@GetMapping("/keywords")   just in case
+    @GetMapping(value = "/keywords", produces = MediaType.APPLICATION_JSON_VALUE)
     public String keywords(){
-        String kwords = nlpservice.getKeywords();
-        return kwords;
+        //String kwords = nlpservice.getKeywords();
+        return nlpservice.getKeywords();
     }
 
-    @GetMapping("/texts")
+    //@GetMapping("/texts")
+    @GetMapping(value = "/texts", produces = MediaType.APPLICATION_JSON_VALUE)
     public String texts(){
-        String texts = nlpservice.getTexts();
-        return texts;
+        //String texts = nlpservice.getTexts();
+        return nlpservice.getTexts();
     }
 
 }
