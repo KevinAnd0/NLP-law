33e38205f1d8e1c39c006e9e7f6409169a278b13
Fliedge <filip.aldenhov@gmail.com>
2021-05-29 18:39:14 +0200

    Fullstack route back in business?

--- Backend/PublicAPIBackend/src/main/java/com/example/nlpPublicAPI/controllers/NlpController.java
+++ Backend/PublicAPIBackend/src/main/java/com/example/nlpPublicAPI/controllers/NlpController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 
 @RestController
+@RequestMapping("/api")
 public class NlpController {
 
     @Autowired
---
+++ Frontend/src/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <form @submit="submit">
+            <h2>Search links</h2>
+            <input type="text" v-model="search_phrase" placeholder="write text">
+            <input type="submit" value="search">
+        </form>
+
+        <div>
+            <h3>{{results}}</h3>
+        </div>
+       
+    </div>
+</template>
+<script>
+export default {
+    methods:{
+        submit(event){
+            event.preventDefault()
+            this.$store.commit('setSearchResults', {
+               search_phrase: this.search_phrase
+            })
+            this.$store.dispatch('getText')
+        
+        }
+        
+    },
+    
+    
+}
+</script>
+
+<style  scoped>
+    form{
+        margin-top: 15px;
+        background-color: grey;
+        border-radius:5px;
+        padding:10px;
+    }
+</style>
