5e59d485ce97cc28783151c9a4be5f28c9efc8a4
Kevin <dosh.90@gmail.com>
2021-05-31 17:36:56 +0200

    Optimized synscraper

--- Backend/NLPBackend/synScraper.py
+++ Backend/NLPBackend/synScraper.py
@@ -3,7 +3,6 @@ import re
 
 def get_syn(query):
     response = requests.get(f'https://www.synonymer.se/sv-syn/{query}')
-    search_tags = re.compile(r'<li value="1">(.+?)</li>')
-    syn_tags = search_tags.findall(response.text)    
-    syns = re.findall('sv-syn/(.+?)">', syn_tags[0])
+    syn_tags = re.findall(r'<li value="1">(.+?)</li>', response.text)    
+    syns = re.findall(r'sv-syn/(.+?)">', syn_tags[0])
     return syns
