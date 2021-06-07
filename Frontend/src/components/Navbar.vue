5220922639d2a85e9cddc7d740c259a4c1192383
Martina <a.martina.eriksson@hotmail.com>
2021-06-07 16:09:21 +0200

    added search field to navbar

--- Frontend/src/components/Navbar.vue
+++ Frontend/src/components/Navbar.vue
@@ -1,14 +1,44 @@
 <template>
   <nav class="navbar " id="navbar">
-        <div class="container-fluid">
-        <span class="navbar-brand mb-0 h1"><router-link to="/" style="text-decoration:none"><h1>DUMSTOL <i class="fas fa-gavel"></i></h1></router-link></span>
+    <div class="container-fluid">
+      <span class="navbar-brand mb-0 h1">
+        <router-link to="/" style="text-decoration:none">
+          <h1>DUMSTOL <i class="fas fa-gavel"></i></h1>
+        </router-link>
+      </span>
+      <!-- <form @submit="submit" class="d-flex w-50">
+        <input class="form-control me-2" type="search" v-model="search_phrase" placeholder="Sök i vägledande domar..." aria-label="Search">
+        <button class="btn btn-outline-secondary" type="submit">Sök</button>
+      </form> -->
+      <!-- <form @submit="submit" class="d-flex w-50">
+            
+        <input type="search" class="form-control me-2" v-model="search_phrase"  placeholder="Sök i vägledande domar...">
       
-      </div>
-    </nav>
+        <button type="submit" class="btn btn-outline-secondary mb-2">Sök</button>
+                
+              
+        </form> -->
+      <SearchField />
+    </div>
+  </nav>
 </template>
-
 <script>
+import SearchField from './SearchField.vue'
 export default {
+  components:{
+    SearchField
+  },
+  // methods:{
+  //       submit(event){
+  //           event.preventDefault()
+  //           this.$store.commit('setSearchResults', {
+  //               search_phrase: this.search_phrase
+  //           })
+  //           this.$store.dispatch('insertSearchPhrase')
+        
+  //       }
+        
+  //   },
 
 }
 </script>
---
+++ Frontend/src/components/SearchField.vue
@@ -0,0 +1,34 @@
+<template>
+    
+    <form @submit="submit" class="d-flex w-50">
+            
+        <input type="search" class="form-control me-2" v-model="search_phrase"  placeholder="Sök i vägledande domar...">
+      
+        <button type="submit" class="btn btn-outline-secondary mb-2">Sök</button>
+                
+              
+    </form>
+    
+</template>
+
+<script>
+export default {
+    methods:{
+        submit(event){
+            event.preventDefault()
+            //this.$router.push('Search') // Should redirect to 'Search.vue'
+            this.$store.commit('setSearchResults', {
+                search_phrase: this.search_phrase
+            })
+            this.$store.dispatch('insertSearchPhrase')
+        
+        }
+        
+    },
+
+}
+</script>
+
+<style>
+
+</style>
