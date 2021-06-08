a14ea53a37ecda0d61c82d6dba3c5df197e1223a
Kevin <dosh.90@gmail.com>
2021-06-08 15:48:44 +0200

    Removed translator

--- Backend/NLPBackend/synScraper.py
+++ Backend/NLPBackend/synScraper.py
@@ -1,6 +1,5 @@
 import requests
 import re
-from google_trans_new import google_translator
 # import spacy
 
 # nlp = spacy.load(r"C:\SISTA PROJEKT ARBETE\sv_pipeline-0.0.0\sv_pipeline-0.0.0\sv_pipeline\sv_pipeline-0.0.0")
@@ -14,24 +13,11 @@ from google_trans_new import google_translator
 #         if(d.is_stop or d.is_punct):
 #             continue
 #         if(d.pos_ in pos_tag):
-#             return d.text
-
-def id_language(query):
-    detector = google_translator()  
-    detect_lang = detector.detect({query})
-     
-    return detect_lang            
-
+#             return d.text          
 
 def get_syn(query):
-    # if id_language(query) != "sv":
-    #     translator = google_translator()
-    #     query = translator.translate(query, lang_tgt="sv")
-        
     response = requests.get(f'https://www.synonymer.se/sv-syn/{query}')
     syn_tags = re.findall(r'<li value="1">(.+?)</li>', response.text)    
     syns = re.findall(r'sv-syn/(.+?)">', syn_tags[0])
     syns.insert(0, query)
     return syns
-    
-
