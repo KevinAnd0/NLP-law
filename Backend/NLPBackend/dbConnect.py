92eff341ef417dd37b288f4b2adbfce62f631d97
Fliedge <filip.aldenhov@gmail.com>
2021-06-08 09:52:40 +0200

    Clean in Spring and Flask. Mostly unused routes.

--- Backend/NLPBackend/dbConnect.py
+++ Backend/NLPBackend/dbConnect.py
@@ -39,41 +39,7 @@ class Database:
         self.db_name = "nlpdatabase.db"
         self.connection = sqlite3.connect(self.db_name)
         self.cur = self.connection.cursor()
-
-    def get_all_rows_in_texts(self):
-        results = []
-        for row in self.cur.execute("SELECT * FROM texts"):
-            obj = {
-                self.cur.description[0][0]: row[0],
-                self.cur.description[1][0]: row[1],
-                self.cur.description[2][0]: row[2]
-            }
-            results.append(obj)
-        return results
-
-    def get_text_by_id(self, id):
-        self.cur.execute("SELECT * FROM texts where id = ?",(id,))
-        result = self.cur.fetchone()
-        return result
-
-    def get_all_keywords(self):
-        results = []
-        for row in self.cur.execute("SELECT * FROM keywords"):
-            obj = {
-                self.cur.description[0][0]: row[0],
-                self.cur.description[1][0]: row[1]
-            }
-            results.append(obj)
-        return results
-
-    def get_specific_keyword(self, word):
-        results = []
-        self.cur.execute("SELECT * FROM keywords WHERE keyword = ?",(word,))
-        rows = self.cur.fetchall()
-        for row in rows:
-            results.append(row)
-        return results
-
+        
     def get_keywords_by_search(self, word):
         results = []
         self.cur.execute("SELECT * FROM keywords WHERE LOWER(keyword) LIKE ('%'||?||'%')", (word,))
@@ -104,5 +70,3 @@ class Database:
         self.connection.commit()
         self.connection.close()
 
-db = Database()
-db.get_texts_by_keywords('')
--- Backend/NLPBackend/nlpAPI.py
+++ Backend/NLPBackend/nlpAPI.py
@@ -7,44 +7,6 @@ from synScraper import get_syn
 
 app = Flask(__name__)
 
-
-@app.route("/texts", methods=["GET"])
-def get_all_text_urls():
-    db = Database()
-    all_rows = db.get_all_rows_in_texts()
-    json_object = jsonify(all_rows)
-    db.close()
-    return json_object
-
-
-
-@app.route("/texts/<int:id>", methods=["GET"])
-def get_text_by_id(id):
-    db = Database()
-    text = db.get_text_by_id(id)
-    JSONobject = jsonify(text)
-    db.close()
-    return JSONobject
-
-
-@app.route("/keywords", methods=["GET"])
-def get_all_keywords():
-    db = Database()
-    keywords = db.get_all_keywords()
-    JSONobject = jsonify(keywords)
-    db.close()
-    return JSONobject
-
-
-@app.route("/keywords/<word>")
-def get_specific_keyword(word):
-    db = Database()
-    keywords = db.get_specific_keyword(word)
-    JSONobject = jsonify(keywords)
-    db.close()
-    return JSONobject
-
-
 @app.route("/search/<phrase>", methods=["GET"])
 def get_text_by_keyword(phrase):
     db = Database()
