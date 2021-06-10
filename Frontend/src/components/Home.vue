2b622b85bf26984dae71933d9fd7a7fb055d878a
Martina <a.martina.eriksson@hotmail.com>
2021-06-10 10:46:15 +0200

    changed element behavior on small screens

--- Frontend/src/components/Home.vue
+++ Frontend/src/components/Home.vue
@@ -7,19 +7,16 @@
             <h2>Vägledande domar för dummies</h2>
         </div>
     
-        <div class="d-none d-sm-block">
-            <div class="row">
-                <div class="col-6 col-sm-6">
-                    <img src="/src/assets/jing-xi-lau-Y5oVH2tNN9U-unsplash.jpg" class="rounded-3 border border-dark w-100">
-                </div>
-                
-                <div class="col-4 col-sm-6 rounded-3">
-                    <h2>För dig som konsument</h2>
-                    <p class="lead "> I vår app har vi valt att fokusera på vägledande domar rörande konsumentfrågor.</p>
-                </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <img src="/src/assets/jing-xi-lau-Y5oVH2tNN9U-unsplash.jpg" class="rounded-3 border border-dark w-100">
+            </div>
+            
+            <div class=" col-lg-6 rounded-3">
+                <h2>För dig som konsument</h2>
+                <p class="lead "> I vår app har vi valt att fokusera på vägledande domar rörande konsumentfrågor.</p>
+            </div>
         </div>
-        
-       </div>
 
     </div>
 </template>
